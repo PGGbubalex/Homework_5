6d989b31762743fe6afd40623b5014d7e6fda572
Igor <99yearago99@gmail.com>
2022-09-28 13:56:19 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
+        int clientOS = 3;
+        int releaseDate = 2011;
 
+        if (clientOS == 0) {
+
+            if (releaseDate<=2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+
+        else if (clientOS == 1) {
+
+            if (releaseDate<=2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+        else {
+            System.out.println("Неизвестный тип устройства");
+        }
     }
 }
+
